638dd5daeb363fff69e0a6bb752d5ce262f2084e
niraj <niraj1998shetty@gmail.com>
2022-01-31 17:48:25 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+
+import "./App.css";
+import Header from "./components/header/Header";
+import CreateProfile from "./components/create/CreateProfile";
+import Contacts from "./components/contacts/Contacts";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
+  const [contacts, setContacts] = useState([]);
+
+  const addContactHandler = (contact) => {
+    console.log(contact);
+    setContacts([...contacts, contact]);
+  };
+
+  const removeContactHandler = (id) => {
+    const newContactList = contacts.filter((contact) => {
+      return contact.id !== id;
+    });
+
+    setContacts(newContactList);
+  };
+
+  useEffect(() => {
+    const retriveContacts = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (retriveContacts) setContacts(retriveContacts);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
+
   return (
     <div>
+      <Header />
+      <CreateProfile addContactHandler={addContactHandler} />
+      <Contacts contacts={contacts} getContactId={removeContactHandler} />
     </div>
   );
 }
